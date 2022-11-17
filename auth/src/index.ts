4680969234b7539e73efccd7582cafc313c78509
David De Los Santos <30784827+dsaints2344@users.noreply.github.com>
2022-11-17 14:48:01 -0400

    Adding cookie-session

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -7,10 +7,19 @@ import { signoutRouter } from "./routes/sign-out";
 import { signupRouter } from "./routes/sign-up";
 import { errorHandler } from "./middlewares/error-handler";
 import { NotFoundError } from "./errors/not-found-error";
+import cookieSession from "cookie-session";
 import mongoose from "mongoose";
 
 const app = express();
+app.set("trust proxy", true);
 app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
+
 app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
