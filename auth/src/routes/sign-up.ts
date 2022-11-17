7f7a556b8a97171e8e1eae7a832b35990f0fb8d8
David De Los Santos <30784827+dsaints2344@users.noreply.github.com>
2022-11-17 15:02:03 -0400

    Implementing jwt inside cookie

--- auth/src/routes/sign-up.ts
+++ auth/src/routes/sign-up.ts
@@ -3,6 +3,7 @@ import { body, validationResult } from "express-validator";
 import { BadRequestError } from "../errors/bad-request-error";
 import { RequestValidationError } from "../errors/request-validation-error";
 import { User } from "../models/user";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
@@ -30,6 +31,21 @@ router.post(
 
     const user = User.build({ email, password });
     await user.save();
+
+    // * Generate JWT
+    const userJwt = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      "fsdfsda"
+    );
+
+    // * store jwt on session
+
+    req.session = {
+      jwt: userJwt,
+    };
 
     res.status(201).send(user);
   }
