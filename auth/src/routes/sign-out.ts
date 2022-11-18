7342959aa13780d760986d98bf6fa6b0bf7b03b6
David De Los Santos <30784827+dsaints2344@users.noreply.github.com>
2022-11-18 17:03:59 -0400

    Sign out user

--- auth/src/routes/sign-out.ts
+++ auth/src/routes/sign-out.ts
@@ -2,6 +2,10 @@ import express from "express";
 
 const router = express.Router();
 
-router.get("/api/users/signout", () => {});
+router.post("/api/users/signout", async (req, res) => {
+  req.session = null;
+
+  res.send({});
+});
 
 export { router as signoutRouter };
