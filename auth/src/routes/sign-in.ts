fd38f37ea887155686bfe7553e35bef943caabbd
David De Los Santos <30784827+dsaints2344@users.noreply.github.com>
2022-11-18 16:46:42 -0400

    Deleting secrets config file

--- auth/src/routes/sign-in.ts
+++ auth/src/routes/sign-in.ts
@@ -1,18 +1,55 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
+import { BadRequestError } from "../errors/bad-request-error";
 import { RequestValidationError } from "../errors/request-validation-error";
 import { validateRequest } from "../middlewares/validate-request";
+import { User } from "../models/user";
+import { Password } from "../services/password";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
-router.get(
+router.post(
   "/api/users/signin",
   [
     body("email").isEmail().withMessage("Email must be valid"),
     body("password").trim().notEmpty().withMessage("You must apply a password"),
   ],
   validateRequest,
-  (req: Request, res: Response) => {}
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      throw new BadRequestError("Invalid credentials");
+    }
+
+    const passwordMatch = await Password.compare(
+      existingUser.password,
+      password
+    );
+
+    if (!passwordMatch) {
+      throw new BadRequestError("wrong password");
+    }
+
+    // * Generate JWT
+    const userJwt = jwt.sign(
+      {
+        id: existingUser.id,
+        email: existingUser.email,
+      },
+      process.env.JWT_KEY!
+    );
+
+    // * store jwt on session
+
+    req.session = {
+      jwt: userJwt,
+    };
+
+    res.status(200).send(existingUser);
+  }
 );
 
 export { router as signinRouter };
--- auth/src/services/password.ts
+++ auth/src/services/password.ts
@@ -8,12 +8,12 @@ export class Password {
     const salt = randomBytes(8).toString("hex");
     const buf = (await scryptAsync(password, salt, 64)) as Buffer;
 
-    return `${buf.toString("hex")}.${salt};`;
+    return `${buf.toString("hex")}.${salt}`;
   }
 
-  static async compare(storePassword: string, supppliedPassword: string) {
-    const [hashedPassword, salt] = storePassword.split(".");
-    const buf = (await scryptAsync(supppliedPassword, salt, 64)) as Buffer;
+  static async compare(storedPassword: string, suppliedPassword: string) {
+    const [hashedPassword, salt] = storedPassword.split(".");
+    const buf = (await scryptAsync(suppliedPassword, salt, 64)) as Buffer;
 
     return buf.toString("hex") === hashedPassword;
   }
