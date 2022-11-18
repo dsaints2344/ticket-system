f35f7c6381340ebc5a2c8e56ab6027102ff724f5
David De Los Santos <30784827+dsaints2344@users.noreply.github.com>
2022-11-18 15:04:19 -0400

    Moving validation request error into new middleware

--- auth/src/routes/sign-in.ts
+++ auth/src/routes/sign-in.ts
@@ -1,7 +1,18 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+import { RequestValidationError } from "../errors/request-validation-error";
+import { validateRequest } from "../middlewares/validate-request";
 
 const router = express.Router();
 
-router.get("/api/users/signin", () => {});
+router.get(
+  "/api/users/signin",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password").trim().notEmpty().withMessage("You must apply a password"),
+  ],
+  validateRequest,
+  (req: Request, res: Response) => {}
+);
 
 export { router as signinRouter };
--- auth/src/routes/sign-up.ts
+++ auth/src/routes/sign-up.ts
@@ -4,6 +4,7 @@ import { BadRequestError } from "../errors/bad-request-error";
 import { RequestValidationError } from "../errors/request-validation-error";
 import { User } from "../models/user";
 import jwt from "jsonwebtoken";
+import { validateRequest } from "../middlewares/validate-request";
 
 const router = express.Router();
 
@@ -16,12 +17,8 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage("Password must be betweem 4 and 20 characters"),
   ],
+  validateRequest,
   async (req: Request, res: Response) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
-    }
-
     const { email, password } = req.body;
     const existingUser = await User.findOne({ email });
 
